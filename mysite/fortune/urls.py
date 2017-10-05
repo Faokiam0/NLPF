7a995ab039bef8260dd0a91c54bb55d3eeeab6e7
Fakum Baccam <faokiam@gmail.com>
2017-10-05 18:05:49 +0200

    Moche et vide manque le form

--- mysite/fortune/urls.py
+++ mysite/fortune/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'fortune'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<fortune_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- mysite/fortune/views.py
+++ mysite/fortune/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Fortune
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the fortune index.")
+    latest_fortune_list = Fortune.objects.order_by('-pub_date')[:30]
+    context = {'latest_fortune_list': latest_fortune_list}
+    return render(request, 'fortune/index.html', context)
+
+def detail(request, fortune_id):
+    try:
+        fortune = Fortune.objects.get(pk=fortune_id)
+    except Fortune.DoesNotExist:
+        raise Http404("Fortune does not exist")
+    return render(request, 'fortune/detail.html', {'fortune': fortune})
